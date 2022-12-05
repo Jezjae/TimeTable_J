915133bef3ff87981faefdb55d3da86388a70572
김재의 <jezjae99@gmail.com>
2022-12-05 17:23:33 +0900

    초기 커밋.

---
+++ model/ClassSignUpMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTable_J
+{
+    public class ClassSignUpMain
+    {
+        public String number;
+
+        public String major;
+
+        public String classNum;
+
+        public String sameClass;
+
+        public String className;
+
+        public String requireOrSelec;
+
+        public String grade;
+
+        public String credit;
+
+        public String time;
+        //쪼개고 넣기 
+
+        public String room;
+
+        public String professor;
+
+        public String language;
+
+
+
+    }
+}
---
+++ view/ScreenBoardZip.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTable_J
+{
+    public class ScreenBoardZip
+    {
+        public void waiting()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("        ♥ ♡ Please wait a moment ♡ ♥");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        public void mainScreen() //메인화면
+        {
+            Console.WriteLine();
+            Console.WriteLine("♥ ♡ ♥ ♡ ♥ ♡ EN:IT UNIV ♡ ♥ ♡ ♥ ♡ ♥");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 1. 수강 신청");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 2. 관심 과목");
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 3. 나의 시간표");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 4. 종 료");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("원하시는 메뉴의 숫자(1~4)를 정확히 입력해주세요. : ");
+
+        }
+
+        public void classSignUpMainScreen()
+        {
+            Console.WriteLine();
+            Console.WriteLine("♥ ♡ ♥ ♡ ♥ ♡ EN:IT UNIV ♡ ♥ ♡ ♥ ♡ ♥");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 1. 수강 강의 추가");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 2. 수강 강의 삭제");
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 3. 수강 강의 조회");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 4. 전체 강의 목록");
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 5. 강의 검색");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 6. 처음 메뉴로 돌아가기");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write(" 원하시는 메뉴의 숫자(1~8)를 입력해주세요. : ");
+        }
+
+        public void classSignUp()
+        {
+            Console.WriteLine();
+            Console.WriteLine("♥ ♡ ♥ ♡ ♥ ♡ ♥ ♡ ♥ ♡ EN:IT UNIV ♡ ♥ ♡ ♥ ♡ ♥ ♡ ♥ ♡ ♥");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 1. 개설 학과 전공으로 검색하여 수강신청");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 2. 학수 번호로 검색하여 수강신청");
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 3. 교과목 명으로 검색하여 수강신청");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 4. 강의 대상 학년으로 검색하여 수강신청");
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 5. 교수명으로 검색하여 수강신청");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 6. 관심 과목으로 검색하여 수강신청");
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 7. 뒤로 가기");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 8. 처음 메뉴로 돌아가기");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write(" 원하시는 메뉴의 숫자(1~8)를 입력해주세요. : ");
+        }
+        public void classSearch()
+        {
+            Console.WriteLine();
+            Console.WriteLine("♥ ♡ ♥ ♡ ♥ ♡ EN:IT UNIV ♡ ♥ ♡ ♥ ♡ ♥");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 1. 개설 학과 전공으로 검색");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 2. 학수 번호로 검색");
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 3. 교과목 명으로 검색");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 4. 강의 대상 학년으로 검색");
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 5. 교수명으로 검색");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 6. 뒤로 가기");
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 7. 종 료");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write(" 원하시는 메뉴의 숫자(1~8)를 입력해주세요. : ");
+        }
+
+        public void interestedSubjectMainScreen()
+        {
+            Console.WriteLine();
+            Console.WriteLine("♥ ♡ ♥ ♡ ♥ ♡ EN:IT UNIV ♡ ♥ ♡ ♥ ♡ ♥");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 1. 관심 과목 추가");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 2. 관심 과목 삭제");
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 3. 관심 과목 조회");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 4. 전체 강의 목록");
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 5. 강의 검색");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 6. 처음 메뉴로 돌아가기");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write(" 원하시는 메뉴의 숫자(1~8)를 입력해주세요. : ");
+        }
+
+        public void interestedSubjectSelect()
+        {
+            Console.WriteLine();
+            Console.WriteLine("♥ ♡ ♥ ♡ ♥ ♡ ♥ ♡ ♥ ♡ EN:IT UNIV ♡ ♥ ♡ ♥ ♡ ♥ ♡ ♥ ♡ ♥");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 1. 개설 학과 전공으로 검색하여 관심 과목 추가");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 2. 학수 번호로 검색하여 관심 과목 추가");
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 3. 교과목 명으로 검색하여 관심 과목 추가");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 4. 강의 대상 학년으로 검색하여 관심 과목 추가");
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 5. 교수명으로 검색하여 관심 과목 추가");
+            Console.WriteLine();
+            Console.WriteLine("           ♡ 6. 뒤로 가기");
+            Console.WriteLine();
+            Console.WriteLine("           ♥ 7. 처음 화면으로 돌아가기");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write(" 원하시는 메뉴의 숫자(1~8)를 입력해주세요. : ");
+        }
+
+
+
+        public void printSubject(ClassSignUpMain allData)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("   ");
+            Console.Write(allData.number);
+            Console.Write(" | ");
+            Console.Write(allData.major);
+            Console.Write(" | ");
+            Console.Write(allData.classNum);
+            Console.Write(" | ");
+            Console.Write(allData.sameClass);
+            Console.Write(" | ");
+            Console.Write(allData.className);
+            Console.Write(" | ");
+            Console.Write(allData.requireOrSelec);
+            Console.Write(" | ");
+            Console.Write(allData.grade);
+            Console.Write(" | ");
+            Console.Write(allData.credit);
+            Console.Write(" | ");
+            Console.Write(allData.time);
+            Console.Write(" | ");
+            Console.Write(allData.room);
+            Console.Write(" | ");
+            Console.Write(allData.professor);
+            Console.Write(" | ");
+            Console.Write(allData.language);
+            Console.Write("   ");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+
+    }
+
+
+
+}
